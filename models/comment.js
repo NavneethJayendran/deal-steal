1f40dd64c8f0a86e1c23b94ad7b4f0764974a9bd
Navneeth Jayendran <navneejay@gmail.com>
2018-08-23 06:58:29 -0700

    username integration fixes
    The 2nd to last commit did not properly replace all <model>.user.username references with <model>.username, causing various pages to fail to load the right data. These were fixed and, to prevent this from happening again, all models now require the username field.

--- models/comment.js
+++ models/comment.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = mongoose.Schema({
-  username: String,
+  username: {
+    type: String, 
+    required: [true, 'someone posted this comment']
+  },
   post: {type: Schema.Types.ObjectId, ref: "Post"},
   creationDate: {type: Date, default: Date()},
   editDate: {type: Date, default: Date()},
   text: String,
-  replyTo: {type: Schema.Types.ObjectId, ref: "Comment"}
+  replyTo: {
+    type: Schema.Types.ObjectId, 
+    ref: "Comment",
+    required: [false, 'can be direct reply to post']
+  }
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
